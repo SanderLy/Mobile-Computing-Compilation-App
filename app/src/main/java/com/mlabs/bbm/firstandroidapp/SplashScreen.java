6e5aa4d937304a1f0e9c54518f2507c0b36bc3b1
Lysander Adriel Gawaran <Lysander Adriel Gawaran>
2016-07-20 15:50:00 +0800

    Initial Commit.

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/SplashScreen.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/SplashScreen.java
@@ -3,32 +3,49 @@ package com.mlabs.bbm.firstandroidapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by benjarmanalili on 16/07/2016.
  */
 public class SplashScreen extends Activity{
-
+    ImageView imgdarkblue, imgpalette;
+    Animation zoom_in,fade_in;
+    TextView txtPalette;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
-    Thread timerThread = new Thread(){
-        public void run(){
-            try{
-                sleep(3000);
+        imgdarkblue = (ImageView) findViewById(R.id.imgdarkblue);
+        imgpalette = (ImageView)findViewById(R.id.imgPalette);
+        txtPalette = (TextView)findViewById(R.id.txtPalette);
+        zoom_in = AnimationUtils.loadAnimation(getBaseContext(),R.anim.zoom_in);
+        fade_in = AnimationUtils.loadAnimation(getBaseContext(),R.anim.fade_in);
+
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    imgdarkblue.startAnimation(zoom_in);
+                    zoom_in.cancel();
+                    txtPalette.setAnimation(fade_in);
+                    imgpalette.startAnimation(fade_in);
+                    sleep(2000);
+                }
+                catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+                finally{
+                    Intent intent = new Intent(SplashScreen.this,login.class );
+                    startActivity(intent);
+                }
             }
-            catch(InterruptedException e){
-                e.printStackTrace();
-            }
-            finally{
-                Intent intent = new Intent(SplashScreen.this,MainActivity.class );
-                startActivity(intent);
-            }
-        }
-    };
-        timerThread.start();
+        };
+            timerThread.start();
     }
 
     @Override
---
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/login.java
@@ -0,0 +1,16 @@
+package com.mlabs.bbm.firstandroidapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Little Garden on 7/20/2016.
+ */
+public class login extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+    }
+}
