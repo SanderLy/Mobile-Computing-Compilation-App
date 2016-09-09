e60f41690638b303b6d5e88ab2c0c3cd1361d5e3
Lysander Adriel Gawaran <glysander22@gmail.com>
2016-09-09 08:09:27 +0800

    on touch event listener activity sept 2 2016

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -1,7 +1,13 @@
 package com.mlabs.bbm.firstandroidapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnTouch;
+        btnTouch = (Button)findViewById(R.id.btnTouch);
+
+        btnTouch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,touch.class );
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
