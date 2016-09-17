8ca72fe005c158a77c0102e4030ba0fcb6f4582a
SanderLy <androidstudio@cl7-20.local>
2016-09-17 18:01:55 +0800

    testing

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/signup.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/signup.java
@@ -1,6 +1,7 @@
 package com.mlabs.bbm.firstandroidapp;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -22,13 +23,30 @@ public class signup extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+        SQLiteDatabase mydatabase = openOrCreateDatabase("dbAccounts",MODE_PRIVATE,null);
         Button btnSignUp ;
-        final EditText etsignEmail, etsignPass;
+        final EditText etsignEmail, etsignPass, etsignConPass;
 
         etsignEmail = (EditText)findViewById(R.id.etsignEmail);
         etsignPass = (EditText)findViewById(R.id.etsignPass);
+        etsignConPass = (EditText)findViewById(R.id.etsignConPass);
         btnSignUp = (Button)findViewById(R.id.btnSignUp);
 
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Pattern.compile("([a-zA-Z0-9]+_?)+@[a-zA-Z0-9]+\\.com").matcher(etsignEmail.getText()).matches()){
+                    if(!(etsignPass.length()== 0)){
+                        if(etsignPass.length()>8 && etsignConPass.getText().equals(etsignEmail)){
+
+                        } else Toast.makeText(getBaseContext(),"Password too short", Toast.LENGTH_SHORT).show();
+                    }else Toast.makeText(getBaseContext(),"Password field is empty", Toast.LENGTH_SHORT).show();
+                }else Toast.makeText(getBaseContext(),"Invalid Email Address", Toast.LENGTH_SHORT).show();
+
+
+                    /*Toast.makeText(getBaseContext(),, Toast.LENGTH_SHORT).show();*/
+            }
+        });
 
 
     }
